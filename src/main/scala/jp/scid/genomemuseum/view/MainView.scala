4c9104947af6c0d6dda2d499848594e8f359137c
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-09-28 17:02:04 +0900

    メインビュークラス作成 refs #98

---
+++ src/main/scala/jp/scid/genomemuseum/view/MainView.scala
@@ -0,0 +1,10 @@
+package jp.scid.genomemuseum.view
+
+import scala.swing.{Swing, BorderPanel}
+import Swing._
+
+class MainView {
+  val contentPane = new BorderPanel {
+    preferredSize = (600, 400)
+  }
+}
