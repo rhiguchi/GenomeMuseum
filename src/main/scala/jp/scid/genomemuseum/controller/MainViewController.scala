8d897abeb9b91e5d2018add53136060e356fe22b
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-09-30 11:42:28 +0900

    メニューにアクションを設定 refs #103

--- src/main/scala/jp/scid/genomemuseum/controller/MainViewController.scala
+++ src/main/scala/jp/scid/genomemuseum/controller/MainViewController.scala
@@ -3,6 +3,7 @@ package jp.scid.genomemuseum.controller
 import jp.scid.genomemuseum.view
 import view.{MainView, MainViewMenuBar}
 import javax.swing.JFrame
+import scala.swing.Action
 
 class MainViewController(
   mainView: MainView,
@@ -10,10 +11,21 @@ class MainViewController(
   menu: MainViewMenuBar
 ) {
   val tableCtrl = new ExhibitTableController(mainView.dataTable)
+  val openAction = Action("Open") { openFile }
+  
+  bindActions(menu)
   
   def showFrame() {
     frameOfMainView.pack
     frameOfMainView setLocationRelativeTo null
     frameOfMainView setVisible true
+  }
+  
+  def openFile() {
+    println("openFile")
+  }
+  
+  def bindActions(menu: MainViewMenuBar) {
+    menu.open.action = openAction
   }
 }
