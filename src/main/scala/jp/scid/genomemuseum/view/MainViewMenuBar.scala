b6f6f87483001507683ad873172f666116418b4b
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-09-30 15:04:40 +0900

    メニューバーにリソース適用 refs #103

--- src/main/scala/jp/scid/genomemuseum/view/MainViewMenuBar.scala
+++ src/main/scala/jp/scid/genomemuseum/view/MainViewMenuBar.scala
@@ -1,5 +1,6 @@
 package jp.scid.genomemuseum.view
 
+import java.util.ResourceBundle
 import scala.swing.{MenuBar, Menu, MenuItem, Separator}
 
 class MainViewMenuBar {
@@ -21,9 +22,39 @@ class MainViewMenuBar {
   val selectAll = new MenuItem("selectAll")
     
   container.contents += (fileMenu, editMenu)
-  
   fileMenu.contents += (open, new Separator, quit)
-  
   editMenu.contents += (undo, new Separator,
     cut, copy, paste, delete, selectAll)
+  
+  // リソース読み込み
+  reloadResources()
+  
+  def reloadResources() {
+    reloadResources(ResourceBundle.getBundle(classOf[MainViewMenuBar].getName))
+  }
+  
+  def reloadResources(res: ResourceBundle) {
+    import collection.JavaConverters._
+    import java.lang.Boolean.parseBoolean
+    import scala.swing.AbstractButton
+    
+    def injectResourceTo(b: AbstractButton, resPrefix: String) {
+      val resKeys = res.getKeys.asScala.filter(_.startsWith(resPrefix)).toList
+      resKeys.foreach { resKey =>
+        resKey.substring(resPrefix.length) match {
+          case ".text" => b.text = res.getString(resKey)
+          case ".enabled" => b.enabled = parseBoolean(res.getString(resKey))
+          case _ => // TODO warnings
+        }
+      }
+    }
+    
+    def injectResourceFromText(btns: AbstractButton*) {
+      btns foreach { btn => injectResourceTo(btn, btn.text) }
+    }
+    
+    injectResourceFromText(fileMenu, editMenu)
+    injectResourceFromText(open, quit)
+    injectResourceFromText(undo, cut, copy, paste, delete, selectAll)
+  }
 }
