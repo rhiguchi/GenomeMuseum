7a5950d0d6316a118a79d335e7242d839fb3508a
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-10-03 08:39:12 +0900

    スキーマ作成 refs #113

--- src/main/scala/jp/scid/genomemuseum/model/MuseumExhibit.scala
+++ src/main/scala/jp/scid/genomemuseum/model/MuseumExhibit.scala
@@ -1,11 +1,16 @@
 package jp.scid.genomemuseum.model
 
 import java.util.Date
+import net.liftweb.mapper._
 
 case class MuseumExhibit(
   var name: String = "",
   var sequenceLength: Int = 0,
   var source: String = "",
   var date: Option[Date] = None
-) {
+) extends LongKeyedMapper[MuseumExhibit] with IdPK {
+  def getSingleton = MuseumExhibit
+}
+
+object MuseumExhibit extends MuseumExhibit("", 0, "", None) with LongKeyedMetaMapper[MuseumExhibit] {
 }
---
+++ src/main/scala/jp/scid/genomemuseum/model/MuseumScheme.scala
@@ -0,0 +1,79 @@
+package jp.scid.genomemuseum.model
+
+import net.liftweb.mapper
+import mapper.{Schemifier}
+
+trait MuseumScheme {
+  
+}
+
+object MuseumScheme {
+  def fromH2Local(location: String): MuseumScheme =
+    new H2DatabaseMuseumScheme(location)
+  def onMemory(): MuseumScheme =
+    new H2DatabaseMuseumScheme("mem:")
+}
+
+private class H2DatabaseMuseumScheme(location: String) extends MuseumScheme {
+  import mapper.{DB, DefaultConnectionIdentifier, Schemifier, MapperRules}
+  val vendor = new DBVendor(location)
+  DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+  Schemifier.schemify(true, Schemifier.neverF _, MuseumExhibit)
+}
+
+import mapper.{ConnectionIdentifier, ConnectionManager}
+import net.liftweb.common.{Box, Empty, Full}
+
+class DBVendor(dbLocation: String = "mem:") extends ConnectionManager {
+  import java.sql.{Connection, DriverManager}
+  
+  private val driverName = "org.h2.Driver"
+
+  private var pool: List[Connection] = Nil
+  private var poolSize = 0
+  private val maxPoolSize = 4
+ 
+  def dbUrl = "jdbc:h2:" + dbLocation
+  
+  private def createOne: Box[Connection] = try {
+    Class.forName(driverName)
+ 
+    val dm = DriverManager.getConnection(dbUrl, "genomemuseum", "genomemuseum")
+ 
+    Full(dm)
+  } catch {
+    case e: Exception => e.printStackTrace; Empty
+  }
+ 
+  def newConnection(name: ConnectionIdentifier): Box[Connection] =
+    synchronized {
+      pool match {
+        case Nil if poolSize < maxPoolSize =>
+          val ret = createOne
+          poolSize = poolSize + 1
+          ret.foreach(c => pool = c :: pool)
+          ret
+ 
+        case Nil => wait(1000L); newConnection(name)
+        case x :: xs => try {
+          x.setAutoCommit(false)
+          Full(x)
+        } catch {
+          case e => try {
+            pool = xs
+            poolSize = poolSize - 1
+            x.close
+            newConnection(name)
+          } catch {
+            case e => newConnection(name)
+          }
+        }
+      }
+    }
+ 
+  def releaseConnection(conn: Connection): Unit = synchronized {
+    pool = conn :: pool
+    notify
+  }
+  
+}
