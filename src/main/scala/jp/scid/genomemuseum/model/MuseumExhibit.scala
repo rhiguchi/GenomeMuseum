d240057a1370b2934084bb9c27a96ebd3fa6b9c1
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-10-05 10:08:10 +0900

    モデルクラスに要素を追加 refs #118

--- src/main/scala/jp/scid/genomemuseum/model/MuseumExhibit.scala
+++ src/main/scala/jp/scid/genomemuseum/model/MuseumExhibit.scala
@@ -6,8 +6,16 @@ import org.squeryl.KeyedEntity
 case class MuseumExhibit(
   var name: String = "",
   var sequenceLength: Int = 0,
+  var accession: String = "",
+  var identifier: String = "",
+  var namespace: String = "",
+  var version: Option[Int] = Some(0),
+  var definition: String = "",
   var source: String = "",
-  var date: Option[Date] = None
+  var organism: String = "",
+  var date: Option[Date] = None,
+  var sequenceUnit: String = "",
+  var moleculeType: String = ""
 ) extends KeyedEntity[Long] {
   def this() = this("")
   var id: Long = 0
