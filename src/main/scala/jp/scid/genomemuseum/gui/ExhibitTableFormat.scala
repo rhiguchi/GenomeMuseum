5d82cd342c66a331d3d05252b34a91d74b5a2e88
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-10-05 10:08:22 +0900

    テーブルフォーマットの変更 refs #118

--- src/main/scala/jp/scid/genomemuseum/gui/ExhibitTableFormat.scala
+++ src/main/scala/jp/scid/genomemuseum/gui/ExhibitTableFormat.scala
@@ -16,11 +16,21 @@ class ExhibitTableFormat extends TableFormat[MuseumExhibit] {
   def getColumnValue(e: MuseumExhibit, column: Int) = column match {
     case 0 => e.name
     case 1 => e.sequenceLength.asInstanceOf[AnyRef]
-    case 2 => e.source
-    case 3 => e.date.map(_.toString).getOrElse("")
+    case 2 => e.accession
+    case 3 => e.identifier
+    case 4 => e.namespace
+    case 5 => e.version.map(e.accession + "." + _).getOrElse("")
+    case 6 => e.definition
+    case 7 => e.source
+    case 8 => e.organism
+    case 9 => e.date.map(_.toString).getOrElse("")
+    case 10 => e.sequenceUnit
+    case 11 => e.moleculeType
   }
 }
 
 object ExhibitTableFormat {
-  val columnNames = Array("name", "sequenceLength", "source", "date")
+  val columnNames = Array("name", "sequenceLength", "accession", "identifier",
+    "namespace", "version", "definition", "source", "organism", "date",
+    "sequenceUnit", "moleculeType")
 }
