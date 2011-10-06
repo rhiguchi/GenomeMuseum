59395c2f5f3a3791b7d7fd1ccc1344824299fc0c
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-10-06 12:52:05 +0900

    文字列用 ComparatorEditor 実装 refs #118

---
+++ src/main/scala/jp/scid/genomemuseum/gui/StringComparatorEditor.scala
@@ -0,0 +1,35 @@
+package jp.scid.genomemuseum.gui
+
+import swing.Publisher
+import java.util.Comparator
+
+/**
+ * 並び替え記述による比較器エディタ
+ */
+class StringComparatorEditor[E](var factory: String => Comparator[E]) extends ComparatorEditor[E] with Publisher {
+  import ComparatorEditor.ComparatorChanged
+  
+  /** 現在の並び替え記述 */
+  private var orderStmt = ""
+  
+  /** 現在の Comparator */
+  private var currentComparator: Comparator[E] = ComparatorEditor.noOrder
+  
+  /** 現在のソート記述をモデルから取得 */
+  def orderStatement = orderStmt
+  
+  def orderStatement_=(newOrder: String) {
+    orderStmt = newOrder
+    updateComparator()
+  }
+  
+  def comparator = currentComparator
+  
+  /**
+   * Comparator を更新する
+   */
+  def updateComparator() {
+    currentComparator = factory(orderStatement)
+    publish(ComparatorChanged(this))
+  }
+}
