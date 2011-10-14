a0eb2e0211203caad0938ce2d740b3b9c052e78d
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-10-14 21:38:52 +0900

    ローカルライブラリファイル管理クラス作成 refs #129

---
+++ src/main/scala/jp/scid/genomemuseum/model/LibraryFileManager.scala
@@ -0,0 +1,135 @@
+package jp.scid.genomemuseum.model
+
+import java.io.{File, FileInputStream, FileOutputStream, IOException}
+import java.net.URI
+
+class LibraryFileManager(baseDir: File) {
+  import LibraryFileManager._
+  
+  if (!baseDir.exists) baseDir.mkdirs
+  
+  val uriScheme = "gmlib"
+  
+  /**
+   * ファイルをライブラリにコピーして保存
+   * {@code exhibit.filePath} に保管場所が適用される
+   */
+  @throws(classOf[IOException])
+  def store(exhibit: MuseumExhibit, source: File) {
+    val dest = getFile(getDefaultStorePathFor(exhibit)) match {
+      case file if file.exists =>
+        findOtherDest(file)
+      case file =>
+        file
+    }
+    
+    fileCopy(source, dest)
+    
+    val libURI = toLibraryURI(dest)
+    exhibit.filePathAsURI = libURI
+  }
+  
+  /**
+   * ライブラリ相対パスから、実際のファイルパスを得る。
+   */
+  def getFile(uri: URI): File = uri.getScheme match {
+    case "file" => new File(uri)
+    case `uriScheme` => new File(baseDir, uri.getPath)
+    case _ => throw new IllegalArgumentException(
+      "Scheme of the uri must be 'file' or '%s' but '%s'".format(uriScheme, uri))
+  }
+  
+  /**
+   * バイオファイルが保存される、標準のファイル名
+   */
+  def getDefaultStorePathFor(exhibit: MuseumExhibit): URI = {
+    import MuseumExhibit.FileType._
+    
+    val namePrefix = exhibit.name match {
+      case "" => exhibit.identifier match {
+        case "" => "untitled"
+        case identifier => identifier
+      }
+      case name => name
+    }
+    
+    val suffix = exhibit.fileType match {
+      case GenBank => "gbk"
+      case FASTA => "fasta"
+      case Unknown => "txt"
+    }
+    
+    val fileName = namePrefix + "." + suffix
+    
+    createLibraryURI(fileName)
+  }
+  
+  /**
+   * ファイルパスから ライブラリ URI を作成。
+   * @throws IllegalArgumentException {@code file} がライブラリディレクトリで開始されていない時
+   */
+  private def toLibraryURI(file: File): URI = {
+    file.getAbsolutePath.startsWith(baseDir.getAbsolutePath) match {
+      case true =>
+        val path = file.getAbsolutePath.substring(baseDir.getAbsolutePath.length)
+        createLibraryURI(path)
+      case false => throw new IllegalArgumentException(
+        "File '%s' cannot convert to library URI.".format(file))
+    }
+  }
+  
+  /**
+   * ライブラリ内パスを表す URI を作成
+   */
+  private def createLibraryURI(path: String): URI = {
+    val pathx = path.dropWhile(File.separatorChar.==)
+    new URI(uriScheme, "/" + pathx, null)
+  }
+}
+
+object LibraryFileManager {
+  /**
+   * ファイルのコピー
+   */
+  @throws(classOf[IOException])
+  private def fileCopy(src: File, dest: File) {
+    import java.nio.channels.FileChannel
+    val srcChannel = new FileInputStream(src).getChannel()
+    val destChannel = new FileOutputStream(dest).getChannel()
+    try {
+      srcChannel.transferTo(0, srcChannel.size(), destChannel)
+    }
+    finally {
+      try {
+        srcChannel.close()
+      }
+      finally {
+        destChannel.close()
+      }
+    }
+  }
+  
+  /**
+   * 連番付けのファイル名探し
+   */
+  private def findOtherDest(base: File): File = {
+    def splitBasename(name: String) = {
+      val index = name.lastIndexOf('.', name.length - 1)
+      if (index > 0) name.splitAt(index)
+      else (name, "")
+    }
+    
+    def findDestFile(dir: File, basename: String, suffix: String, count: Int = 1): File = {
+      val candidate = new File(dir, basename + " " + count + suffix)
+      candidate.createNewFile match {
+        case true => candidate
+        case false => findDestFile(dir, basename, suffix, count + 1)
+      }
+    }
+    
+    val dir = base.getParentFile
+    val (basename, suffix) = splitBasename(base.getName)
+    
+    findDestFile(dir, basename, suffix)
+  }
+}
---
+++ src/test/scala/jp/scid/genomemuseum/model/LibraryFileManagerSpec.scala
@@ -0,0 +1,112 @@
+package jp.scid.genomemuseum.model
+
+import org.specs2._
+import java.io.{File, IOException}
+import java.net.URI
+import MuseumExhibit.FileType._
+
+class LibraryFileManagerSpec extends Specification {
+  def is = if (tryCreateTempDir.isEmpty) "LibraryFileManager" ^
+    "To create temporary directory was not allowed." ^ skipped
+  else "LibraryFileManager" ^
+    "getDefaultStorePathFor" ^
+      "MuseumExhibit.name が含まれる" ! pathFor.s1 ^
+      "name が空白の時は identifier を使用" ! pathFor.s2 ^
+      "name と identifier が空白の時は untitled" ! pathFor.s3 ^
+      "fileType が GenBank の拡張子は .gbk" ! pathFor.s4 ^
+      "fileType が FASTA の拡張子は .fasta" ! pathFor.s5 ^
+      "fileType が Unknown の拡張子は .txt" ! pathFor.s6 ^
+    bt ^ "getFile" ^
+      "URI のパス部分と File の絶対パスの終端が一致" ! getFile.s1 ^
+      "ライブラリのパスから開始されている" ! getFile.s2 ^
+      "ライブラリスキーマでない URI は例外" ! getFile.s3 ^
+      "file スキーマは、File に変換される" ! getFile.s4 ^
+    bt ^ "store" ^
+      "MuseumExhibit にパスが適用される" ! store.s1 ^
+      "ライブラリ内にファイルがコピーされる" ! store.s2 ^
+      "標準の保存先にすでにファイルがある時は、拡張子前に連番でリネームされる" ! store.s3
+  
+  trait TestBase {
+    val baseDir = tryCreateTempDir.get
+    val manager = new LibraryFileManager(baseDir)
+    
+    protected def pathFor(e: MuseumExhibit) = manager.getDefaultStorePathFor(e)
+  }
+  
+  def pathFor = new TestBase {
+    
+    def s1 = pathFor(MuseumExhibit("exhibit")).getPath must contain("exhibit")
+    
+    def s2 = pathFor(MuseumExhibit("", identifier = "1234")).getPath must contain("1234")
+    
+    def s3 = pathFor(MuseumExhibit("")).getPath must contain("untitled")
+    
+    def s4 = pathFor(MuseumExhibit("a", fileType = GenBank))
+      .getPath must endWith("/a.gbk")
+    
+    def s5 = pathFor(MuseumExhibit("b", fileType = FASTA))
+      .getPath must endWith("/b.fasta")
+    
+    def s6 = pathFor(MuseumExhibit("c", fileType = Unknown))
+      .getPath must endWith("/c.txt")
+  }
+  
+  def getFile = new TestBase {
+    val uri = new URI(manager.uriScheme, "/path/to/file", null)
+    val invalidSchemeURI = new URI(manager.uriScheme + "x", "/path/to/file", null)
+    val fileSchema = new URI("file", "/path/to/file", null)
+    
+    def s1 = manager.getFile(uri).getAbsolutePath must endWith("/path/to/file")
+    
+    def s2 = manager.getFile(uri).getAbsolutePath must
+      startWith(baseDir.getAbsolutePath)
+    
+    def s3 = manager.getFile(invalidSchemeURI) must throwA[IllegalArgumentException]
+    
+    def s4 = manager.getFile(fileSchema).getAbsolutePath must_== "/path/to/file"
+  }
+  
+  def store = new TestBase {
+    val e = MuseumExhibit("exhibit", fileType = GenBank)
+    val file = File.createTempFile("testTemp", ".gbk")
+    
+    val e2 = MuseumExhibit("exhibit", fileType = GenBank)
+    val e3 = MuseumExhibit("exhibit", fileType = GenBank)
+    
+    assert(e.filePath == "")
+    manager.store(e, file)
+    manager.store(e2, file)
+    manager.store(e3, file)
+    
+    val libraryFile = manager.getFile(e.filePathAsURI)
+    
+    val e2File = manager.getFile(e2.filePathAsURI)
+    val e3File = manager.getFile(e3.filePathAsURI)
+    
+    def s1 = e.filePath must_!= ""
+    
+    def s2 = libraryFile must beAFile
+    
+    def s3_2 = e3File.getPath must endWith("/exhibit 2.gbk")
+    def s3 = e2File.getPath must be_!=(libraryFile.getPath) and
+      endWith("/exhibit 1.gbk") and s3_2
+  }
+  
+  @throws(classOf[IOException])
+  def tryCreateTempDir: Option[File] = {
+    try {
+      Some(createTempDir)
+    }
+    catch {
+      case e: SecurityException => None
+    }
+  }
+  
+  @throws(classOf[IOException])
+  def createTempDir = {
+    val tempFile = File.createTempFile("LibraryFileManagerSpec", "")
+    tempFile.delete
+    tempFile.mkdir
+    tempFile
+  }
+}
