35cd473b3ac301df1002759c821b9fe314cc46b0
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-10-14 15:56:19 +0900

    エンティティをファイル保存用に変更 refs #129

--- src/main/scala/jp/scid/genomemuseum/model/MuseumExhibit.scala
+++ src/main/scala/jp/scid/genomemuseum/model/MuseumExhibit.scala
@@ -1,6 +1,7 @@
 package jp.scid.genomemuseum.model
 
 import java.util.Date
+import java.net.URI
 import org.squeryl.KeyedEntity
 
 case class MuseumExhibit(
@@ -9,14 +10,31 @@ case class MuseumExhibit(
   var accession: String = "",
   var identifier: String = "",
   var namespace: String = "",
-  var version: Option[Int] = Some(0),
+  var version: Option[Int] = None,
   var definition: String = "",
   var source: String = "",
   var organism: String = "",
   var date: Option[Date] = None,
   var sequenceUnit: String = "",
-  var moleculeType: String = ""
+  var moleculeType: String = "",
+  var filePath: String = "",
+  var fileSize: Long = 0,
+  var fileType: MuseumExhibit.FileType.Value = MuseumExhibit.FileType.Unknown
 ) extends KeyedEntity[Long] {
-  def this() = this("")
+  def this() = this("", version = Some(0))
   var id: Long = 0
+  
+  def filePathAsURI = URI.create(filePath)
+  def filePathAsURI_=(uri: URI) {
+    filePath = uri.toString
+  }
+}
+
+object MuseumExhibit {
+  object FileType extends Enumeration {
+    type FileType = Value
+    val Unknown = Value(0, "Unknown")
+    val GenBank = Value(1, "GenBank")
+    val FASTA = Value(2, "FASTA")
+  }
 }
