b26293fecaaad460c96c43f6670032a3d394844b
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-10-18 00:02:36 +0900

    フィルタリングの動作を変更 refs #133

--- src/main/scala/jp/scid/genomemuseum/gui/ExhibitTableModel.scala
+++ src/main/scala/jp/scid/genomemuseum/gui/ExhibitTableModel.scala
@@ -3,7 +3,8 @@ package jp.scid.genomemuseum.gui
 import java.util.Date
 
 import ca.odell.glazedlists.gui.TableFormat
-import ca.odell.glazedlists.swing.SearchEngineTextFieldMatcherEditor
+import ca.odell.glazedlists.TextFilterator
+import ca.odell.glazedlists.matchers.SearchEngineTextMatcherEditor
 
 import jp.scid.gui.table.DataTableModel
 import jp.scid.genomemuseum.model.{MuseumExhibit, ExhibitDataService, ExhibitRoom,
@@ -14,10 +15,24 @@ class ExhibitTableModel(tableFormat: TableFormat[MuseumExhibit]) extends DataTab
   import javax.swing.JTextField
   import collection.mutable.Buffer
   
+  val textMatcher = new SearchEngineTextMatcherEditor(new TextFilterator[MuseumExhibit] {
+    def getFilterStrings(baseList: java.util.List[String], e: MuseumExhibit) {
+      baseList add e.name
+      baseList add e.source
+    }
+  })
+  
+  filterWith(textMatcher)
+  
   def this() = this(new ExhibitTableFormat)
   
   def filterUsing(field: JTextField) {
     filterUsing(field, getFilterStrings _)
+  }
+  
+  /** フィルタリングしなおし */
+  def refilterWith(text: String) {
+    textMatcher refilter text
   }
   
   /** フィルタリング対象文字列を取得 */
