dc9e0b77587b251fe99118c29c790bb85955591a
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2011-11-25 11:16:22 +0900

    メインビューの更新

--- src/main/java/jp/scid/genomemuseum/view/MainView.java
+++ src/main/java/jp/scid/genomemuseum/view/MainView.java
@@ -1,6 +1,10 @@
 package jp.scid.genomemuseum.view;
 
+import static javax.swing.BorderFactory.*;
+import static javax.swing.SpringLayout.*;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -10,6 +14,7 @@ import java.beans.PropertyChangeListener;
 
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -19,12 +24,14 @@ import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 import javax.swing.JTree;
+import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.event.PopupMenuEvent;
@@ -40,6 +47,8 @@ import com.explodingpixels.macwidgets.MacWidgetFactory;
 import com.explodingpixels.macwidgets.UnifiedToolBar;
 
 public class MainView implements GenomeMuseumView {
+    private static final Color COLOR_ACTIVITY_FOREGROUND = new Color(0f, 0f, 0f, 0.8f);
+    
     private final ComponentFactory factory = new ComponentFactory();
     private final static Icon loadingIcon = new ImageIcon(MainView.class.getResource("loading.gif"));
     
@@ -77,6 +86,7 @@ public class MainView implements GenomeMuseumView {
 //        loadingIconLabel.setPreferredSize(new Dimension(24, 24));
     }
 
+    // Source List Area
     public final SourceListCellEditor sourceListCellEditor = new SourceListCellEditor();
     public final JTree sourceList = new JTree(); {
         sourceList.setUI(new SourceListTreeUI());
@@ -88,14 +98,76 @@ public class MainView implements GenomeMuseumView {
         sourceList.setEditable(true);
 
     }
-    public final JScrollPane sourceListScroll = MacWidgetFactory.createSourceListScrollPane(sourceList);
+    public final JScrollPane sourceListScroll = MacWidgetFactory.createSourceListScrollPane(sourceList); {
+        sourceListScroll.setPreferredSize(new Dimension(260, 260));
+        sourceListScroll.setMinimumSize(new Dimension(120, 260));
+    }
+    
+    // Activity Area
+    // - Loading status
+    public final JProgressBar fileLoadingProgress = new JProgressBar();
+    public final JLabel fileLoadingStatus = new JLabel("Loading Status"); {
+        fileLoadingStatus.setFont(fileLoadingStatus.getFont().deriveFont(11f));
+        fileLoadingStatus.setMinimumSize(new Dimension(0, 0));
+        fileLoadingStatus.setForeground(COLOR_ACTIVITY_FOREGROUND);
+        fileLoadingStatus.setHorizontalAlignment(SwingConstants.RIGHT);
+    }
+    public final JComponent fileLoadingActivityPane = new JPanel(); {
+        JComponent parent = fileLoadingActivityPane;
+        parent.setOpaque(false);
+        
+        SpringLayout layout = new SpringLayout();
+        parent.setLayout(layout);
+        
+        parent.add(fileLoadingProgress);
+        layout.putConstraint(WEST, fileLoadingProgress, 0, WEST, parent);
+        layout.putConstraint(NORTH, fileLoadingProgress, 6, NORTH, parent);
+        layout.putConstraint(EAST, fileLoadingProgress, 0, EAST, parent);
+        
+        parent.add(fileLoadingStatus);
+        layout.putConstraint(WEST, fileLoadingStatus, 0, WEST, parent);
+        layout.putConstraint(NORTH, fileLoadingStatus, 6, SOUTH, fileLoadingProgress);
+        layout.putConstraint(EAST, fileLoadingStatus, 0, EAST, parent);
+        
+        layout.putConstraint(SOUTH, parent, 6, SOUTH, fileLoadingStatus);
+    }
+    
+    public final JLabel activityAreaTitle = new JLabel("GenomeMuseum Activity"); {
+        activityAreaTitle.setFont(activityAreaTitle.getFont().deriveFont(11f).deriveFont(Font.BOLD));
+        activityAreaTitle.setForeground(COLOR_ACTIVITY_FOREGROUND);
+        activityAreaTitle.setMaximumSize(new Dimension(Integer.MAX_VALUE, 20));
+        activityAreaTitle.setMinimumSize(new Dimension(0, 0));
+    }
+    /** Activity Pane */
+    public final JComponent activityPane = new JPanel(); {
+        JComponent parent = activityPane;
+        parent.setOpaque(false);
+        parent.setBorder(createCompoundBorder(
+                createMatteBorder(1, 0, 0, 0, new Color(0f, 0f, 0f, 0.3f)),
+                createEmptyBorder(4, 8, 0, 8)));
+        parent.setPreferredSize(new Dimension(500, 100));
+        
+        parent.setLayout(new BoxLayout(parent, BoxLayout.PAGE_AXIS));
+        
+        parent.add(activityAreaTitle);
+        parent.add(fileLoadingActivityPane);
+    }
+    
+    /** EAST Area */
+    public final JComponent eastAreaPane = new JPanel(new BorderLayout()); {
+        JComponent parent = eastAreaPane;
+        parent.setOpaque(false);
+        
+        parent.add(sourceListScroll, "Center");
+        parent.add(activityPane, "South");
+    }
     
     // Top
     private final UnifiedToolBar toolBarView = new UnifiedToolBar();
     public final JComponent toolBarPane = toolBarView.getComponent(); {
         toolBarView.addComponentToRight(statusLabel);
-        toolBarView.addComponentToRight(quickSearchField);
         toolBarView.addComponentToRight(loadingIconLabel);
+        toolBarView.addComponentToRight(quickSearchField);
     }
     
     // Bottom left
@@ -143,12 +215,15 @@ public class MainView implements GenomeMuseumView {
         fComponentBottomBar.getComponent().setPreferredSize(new Dimension(-1, 24));
     }
     public final JPanel sourceListPane = new JPanel(new BorderLayout()); {
-        sourceListPane.add(sourceListScroll, "Center");
+        sourceListPane.add(eastAreaPane, "Center");
         sourceListPane.add(fComponentBottomBar.getComponent(), "South");
     }
     public final JSplitPane sourceListDataTableSplit = new JSplitPane(
-            JSplitPane.HORIZONTAL_SPLIT, true,
-            sourceListPane, dataListContentSplit);
+            JSplitPane.HORIZONTAL_SPLIT, true, sourceListPane,
+            dataListContentSplit);
+    {
+        sourceListDataTableSplit.setBorder(null);
+    }
 
     public final JPanel contentPane = new JPanel(new BorderLayout()); {
         contentPane.add(toolBarPane, "North");
