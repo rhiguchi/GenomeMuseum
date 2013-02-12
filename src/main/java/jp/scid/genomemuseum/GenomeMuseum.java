83af2683b8d0126512ebc03a807b83a0179a4908
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2013-02-12 18:08:43 +0900

    ログ取り方法を修正

--- src/main/java/jp/scid/genomemuseum/GenomeMuseum.java
+++ src/main/java/jp/scid/genomemuseum/GenomeMuseum.java
@@ -1,41 +1,15 @@
 package jp.scid.genomemuseum;
 
-import java.io.UnsupportedEncodingException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.jdesktop.application.Application;
 
 public class GenomeMuseum {
+    private static Logger logger = Logger.getLogger(GenomeMuseum.class.getPackage().getName());
+    
     public static void main(String[] args) {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
         System.setProperty("com.apple.mrj.application.apple.menu.about.name", "GenomeMuseum");
-        
-        Logger logger = Logger.getLogger(GenomeMuseum.class.getPackage().getName());
-        if (logger.getHandlers().length == 0) {
-            ConsoleHandler h = new ConsoleHandler();
-            h.setFormatter(new SimpleFormatter());
-            h.setLevel(Level.ALL);
-            try {
-                h.setEncoding("UTF-8");
-            }
-            catch (SecurityException e) {
-                throw new IllegalStateException(e);
-            }
-            catch (UnsupportedEncodingException e) {
-                throw new IllegalStateException(e);
-            }
-            
-            logger.addHandler(h);
-            logger.setLevel(Level.FINEST);
-            logger.setUseParentHandlers(false);
-            
-            Logger guilogger = Logger.getLogger("jp.scid.gui");
-            guilogger.addHandler(h);
-            guilogger.setLevel(Level.INFO);
-            guilogger.fine("GUI logger is prepared.");
-        }
         
         logger.info("Welcome to GenomeMuseum.");
         
