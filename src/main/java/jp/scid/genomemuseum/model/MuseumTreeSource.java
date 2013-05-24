6aa3085b694b1bc089696ef9432e589c0592ac1b
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2013-05-24 13:59:59 +0900

    フォルダ用モデルの作成

---
+++ src/main/java/jp/scid/genomemuseum/model/MuseumTreeSource.java
@@ -0,0 +1,174 @@
+package jp.scid.genomemuseum.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+import javax.swing.ListModel;
+
+import jp.scid.bio.store.SequenceLibrary;
+import jp.scid.bio.store.folder.GroupFolder;
+
+import com.explodingpixels.widgets.TextProvider;
+
+public class MuseumTreeSource implements NodeListTreeModel.TreeSource {
+    private final RootItemList rootItemList;
+    
+    private SequenceLibrary sequenceLibrary;
+    
+    public MuseumTreeSource() {
+        rootItemList = new RootItemList();
+    }
+    
+    @Override
+    public boolean getAllowsChildren(Object obj) {
+        if (obj instanceof Category) {
+            return true;
+        }
+        else if (obj instanceof SequenceLibrary || obj instanceof GroupFolder) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public ListModel getChildren(Object parent) {
+        if (parent == this) {
+            return rootItemList;
+        }
+        else if (parent instanceof Category) {
+            if (parent == rootItemList.userCollections) {
+                return sequenceLibrary.getRootFolderList();
+            }
+            return ((Category) parent).children();
+        }
+        else if (parent instanceof SequenceLibrary) {
+            return ((SequenceLibrary) parent).getRootFolderList();
+        }
+        else if (parent instanceof GroupFolder) {
+            return ((GroupFolder) parent).getChildFolders();
+        }
+        return null;
+    }
+    
+    public void setSequenceLibrary(SequenceLibrary sequenceLibrary) {
+        rootItemList.setLocalFilesSource(sequenceLibrary);
+        rootItemList.setUserCollectionsRoot(sequenceLibrary);
+        this.sequenceLibrary = sequenceLibrary;
+    }
+
+    private static class RootItemList extends AbstractListModel {
+        private static final String DEFAULT_LIBRARIES_NAME = "Libraries";
+        private static final String DEFAULT_COLLECTIONS_NAME = "Collections";
+        private static final String DEFAULT_LOCAL_FILES_NAME = "Local Files";
+        
+        private final Category librariesNode;
+        private Category userCollections;
+        
+        private LeafElement localFilesSourceNode;
+        
+        private List<Object> items = new ArrayList<Object>();
+        
+        RootItemList() {
+            librariesNode = new Category(DEFAULT_LIBRARIES_NAME, 0);
+            items.add(librariesNode);
+        }
+        
+        public void setLocalFilesSource(Object source) {
+            if (localFilesSourceNode != null) {
+                librariesNode.removeChild(localFilesSourceNode);
+                localFilesSourceNode = null;
+            }
+            
+            if (source != null) {
+                localFilesSourceNode = new LeafElement(DEFAULT_LOCAL_FILES_NAME, source);
+                librariesNode.addChild(localFilesSourceNode);
+            }
+        }
+        
+        public void setUserCollectionsRoot(Object source) {
+            if (userCollections != null) {
+                items.remove(userCollections);
+                userCollections = null;
+            }
+            
+            if (source != null) {
+                userCollections = new Category(DEFAULT_COLLECTIONS_NAME, 10);
+                items.add(userCollections);
+            }
+        }
+        
+        @Override
+        public int getSize() {
+            return items.size();
+        }
+
+        @Override
+        public Object getElementAt(int index) {
+            return items.get(index);
+        }
+    }
+    
+    private static abstract class TreeElement implements TextProvider {
+        private final String name;
+        
+        public TreeElement(String name) {
+            this.name = name;
+        }
+        
+        @Override
+        public String getText() {
+            return name;
+        }
+    }
+    
+    public static class Category extends TreeElement implements TextProvider, Comparable<Category> {
+        private final DefaultListModel children;
+        private final int order;
+        
+        Category(String name, int order) {
+            super(name);
+            this.order = order;
+            children = new DefaultListModel();
+        }
+        
+        @Override
+        public int compareTo(Category o) {
+            if (this.order < o.order) {
+                return -1;
+            }
+            else if (this.order > o.order) {
+                return 1;
+            }
+            
+            return this.getText().compareTo(o.getText());
+        }
+        
+        void addChild(Object object) {
+            children.addElement(object);
+        }
+
+        void removeChild(Object element) {
+            children.removeElement(element);
+        }
+        
+        public ListModel children() {
+            return children;
+        }
+    }
+
+    public static class LeafElement extends TreeElement {
+        private final Object nodeObject;
+        
+        LeafElement(String name, Object nodeObject) {
+            super(name);
+            this.nodeObject = nodeObject;
+        }
+        
+        LeafElement(String name) {
+            this(name, null);
+        }
+    }
+}
+
---
+++ src/main/java/jp/scid/genomemuseum/model/NodeListTreeModel.java
@@ -0,0 +1,269 @@
+package jp.scid.genomemuseum.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+
+public class NodeListTreeModel implements TreeModel {
+
+    private final DefaultTreeModel delegate;
+    
+    private final Map<Object, ChildrenSync> childrenSyncs;
+    
+    private final Map<TreeModelListener, TreeModelListenerProxy> listenerProxyMap;
+    
+    protected TreeSource treeSource;
+    
+    public NodeListTreeModel(DefaultTreeModel delegate) {
+        if (delegate == null) throw new IllegalArgumentException("delegate must not be null");
+        this.delegate = delegate;
+        
+        childrenSyncs = new HashMap<Object, ChildrenSync>();
+        listenerProxyMap = new HashMap<TreeModelListener, TreeModelListenerProxy>();
+    }
+
+    public NodeListTreeModel() {
+        this(new DefaultTreeModel(null, true));
+    }
+    
+    // treeModel
+    public Object getRoot() {
+        return delegate.getRoot();
+    }
+
+    public int getIndexOfChild(Object parent, Object child) {
+        ensureChildrenRetrieved(parent);
+        return delegate.getIndexOfChild(parent, child);
+    }
+
+    public Object getChild(Object parent, int index) {
+        ensureChildrenRetrieved(parent);
+        return delegate.getChild(parent, index);
+    }
+
+    public int getChildCount(Object parent) {
+        ensureChildrenRetrieved(parent);
+        return delegate.getChildCount(parent);
+    }
+
+    public boolean isLeaf(Object node) {
+        return delegate.isLeaf(node);
+    }
+
+    public void valueForPathChanged(TreePath path, Object newValue) {
+        delegate.valueForPathChanged(path, newValue);
+    }
+
+    @Override
+    public void addTreeModelListener(TreeModelListener listener) {
+        TreeModelListenerProxy proxy = new TreeModelListenerProxy(listener);
+        listenerProxyMap.put(listener, proxy);
+        delegate.addTreeModelListener(proxy);
+    }
+    
+    @Override
+    public void removeTreeModelListener(TreeModelListener listener) {
+        TreeModelListenerProxy proxy = listenerProxyMap.remove(listener);
+        if (proxy != null) {
+            delegate.removeTreeModelListener(proxy);
+        }
+    }
+    
+    private void ensureChildrenRetrieved(Object parent) {
+        if (childrenSyncs.containsKey(parent)) {
+            return;
+        }
+        
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) parent;
+        ChildrenSync sync = new ChildrenSync(node);
+        ListModel children = getChildren(node.getUserObject());
+        sync.setListModel(children);
+        
+        childrenSyncs.put(parent, sync);
+    }
+
+    protected DefaultMutableTreeNode createTreeNode(Object nodeObject, boolean allowsChildren) {
+        return new DefaultMutableTreeNode(nodeObject, allowsChildren);
+    }
+    
+    // treeSource
+    public TreeSource getTreeSource() {
+        return treeSource;
+    }
+    
+    public void setTreeSource(TreeSource treeSource) {
+        this.treeSource = treeSource;
+        for (Entry<Object, ChildrenSync> entry: childrenSyncs.entrySet()) {
+            entry.getValue().dispose();
+        }
+        
+        MutableTreeNode root;
+        if (treeSource == null) {
+            root = null;
+        }
+        else {
+            root = createTreeNode(treeSource, true);
+        }
+        
+        delegate.setRoot(root);
+    }
+    
+    private boolean getAllowsChildren(Object nodeObject) {
+        if (treeSource == null) {
+            return false;
+        }
+        return treeSource.getAllowsChildren(nodeObject);
+    }
+    
+    private ListModel getChildren(Object parent) {
+        return treeSource.getChildren(parent);
+    }
+    
+    public static interface TreeSource {
+        boolean getAllowsChildren(Object nodeObject);
+        
+        ListModel getChildren(Object parent);
+    }
+    
+    private class ChildrenSync implements ListDataListener {
+        private final DefaultMutableTreeNode node;
+        private ListModel model;
+        
+        public ChildrenSync(DefaultMutableTreeNode node) {
+            if (node == null) throw new IllegalArgumentException("node must not be null");
+            this.node = node;
+        }
+
+        public void dispose() {
+            if (this.model != null) {
+                this.model.removeListDataListener(this);
+            }
+        }
+
+        public void setListModel(ListModel newModel) {
+            if (this.model != null) {
+                this.model.removeListDataListener(this);
+            }
+            this.model = newModel;
+            reload();
+            
+            if (newModel != null) {
+                newModel.addListDataListener(this);
+            }
+        }
+
+        private void reload() {
+            node.removeAllChildren();
+            
+            if (model != null) {
+                insertChildren(model, 0, model.getSize());
+            }
+        }
+
+        private void insertChildren(ListModel source, int start, int end) {
+            for (int i = start; i < end; i++) {
+                Object nodeObject = source.getElementAt(i);
+                boolean allowsChildren = getAllowsChildren(nodeObject);
+                MutableTreeNode childNode = createTreeNode(nodeObject, allowsChildren);
+                
+                node.insert(childNode, i);
+            }
+        }
+        
+        @Override
+        public void intervalAdded(ListDataEvent e) {
+            int start = e.getIndex0();
+            int end = e.getIndex1();
+            
+            insertChildren((ListModel) e.getSource(), start, end + 1);
+            int[] childIndices = range(start, end);
+            delegate.nodesWereInserted(node, childIndices);
+        }
+
+        @Override
+        public void intervalRemoved(ListDataEvent e) {
+            int start = e.getIndex0();
+            int end = e.getIndex1();
+            
+            TreeNode[] removedChildren = new TreeNode[end - start + 1];
+            
+            for (int i = end; i >= start; i--) {
+                removedChildren[i - start]  = node.getChildAt(i);
+                node.remove(i);
+            }
+
+            int[] childIndices = range(start, end);
+            delegate.nodesWereRemoved(node, childIndices, removedChildren);
+        }
+
+        @Override
+        public void contentsChanged(ListDataEvent e) {
+            ListModel source = (ListModel) e.getSource();
+            int start = e.getIndex0();
+            int end = e.getIndex1();
+            
+            for (int i = start; i <= end; i++) {
+                Object newNodeObject = source.getElementAt(i);
+                MutableTreeNode childNode = (MutableTreeNode) node.getChildAt(i);
+                childNode.setUserObject(newNodeObject);
+            }
+            
+            int[] childIndices = range(start, end);
+            delegate.nodesChanged(node, childIndices);
+        }
+    }
+
+    private class TreeModelListenerProxy implements TreeModelListener {
+        private final TreeModelListener original;
+        
+        public TreeModelListenerProxy(TreeModelListener original) {
+            this.original = original;
+        }
+
+        private TreeModelEvent wrapEvent(TreeModelEvent e) {
+            TreeModelEvent realEvent =
+                    new TreeModelEvent(NodeListTreeModel.this, e.getTreePath(), e.getChildIndices(), e.getChildren());
+            return realEvent;
+        }
+        
+        @Override
+        public void treeNodesChanged(TreeModelEvent e) {
+            original.treeNodesChanged(wrapEvent(e));
+        }
+
+        @Override
+        public void treeNodesInserted(TreeModelEvent e) {
+            original.treeNodesInserted(wrapEvent(e));
+        }
+
+        @Override
+        public void treeNodesRemoved(TreeModelEvent e) {
+            original.treeNodesRemoved(wrapEvent(e));
+        }
+
+        @Override
+        public void treeStructureChanged(TreeModelEvent e) {
+            original.treeStructureChanged(wrapEvent(e));
+        }
+    }
+    
+    private static int[] range(int start, int end) {
+        int[] childIndices = new int[end - start + 1];
+        for (int i = 0; i < childIndices.length; i++) {
+            childIndices[i] = i + start;
+        }
+        return childIndices;
+    }
+}
